12d44e0609b57cd1df7050b4ace516e1cb3499ae
fidelisepeter <fidelisepeter@gmail.com>
2022-10-25 12:15:20 +0100

    initial commit to dev

--- src/Wassenger/Wassenger.php
+++ src/Wassenger/Wassenger.php
@@ -1,14 +1,15 @@
 <?php
 namespace Alresia\LaravelWassenger;
 
+use Alresia\LaravelWassenger\Traits\Messages;
 use Alresia\LaravelWassenger\Traits\WassengerRequest;
 
 
 /**
  * @internal
  */
-class Wassenger
+class Wassenger3
 {
-    use WassengerRequest;
+    use WassengerRequest, Messages;
     
 }
